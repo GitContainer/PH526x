fb6bee139755bbdf0ba48b1a3cbd46a0dbd0c7bd
ahasani <albin.hasani@ikubinfo.al>
2017-11-11 00:10:49 +0100

    Exercise 1c

---
+++ homework/week1/Exercise_1c.py
@@ -0,0 +1,21 @@
+import string
+
+alphabet = string.ascii_letters
+
+sentence = 'Jim quickly realized that the beautiful gowns are expensive'
+
+
+# Create your function here!
+def counter(input_string):
+    count_letters = {}
+    for letter in input_string:
+        if letter in alphabet:
+            if not letter in count_letters:
+                count_letters[letter] = 1
+            else:
+                count_letters[letter] += 1
+    return count_letters
+
+
+count_letters = counter(sentence)
+print(count_letters.items())
