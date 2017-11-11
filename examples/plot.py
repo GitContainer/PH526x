76d29cc127e666d853ab2b01ffc159f40374c040
ahasani <albin.hasani@ikubinfo.al>
2017-11-11 16:11:07 +0100

    add plot for week2

---
+++ examples/plot.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 20)
+y1 = x ** 2.0
+y2 = x ** 1.5
+plt.plot(x, y1, "bo-", linewidth=2, markersize=12, label="First")
+plt.plot(x, y2, "bo-", linewidth=2, markersize=12, label="Second")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.axis([-0.5, 10.5, -5, 105])
+plt.legend(loc="upper left")
+plt.show()
+plt.savefig("myplot.pdf")
