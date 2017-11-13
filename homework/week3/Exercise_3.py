38d9169b89b76c5791c1a0ee6d1ea4a227a4050e
ahasani <albin.hasani@ikubinfo.al>
2017-11-13 23:41:29 +0100

    Refactor Exercise 3 Week 3

--- homework/week3/Exercise_3.py
+++ homework/week3/Exercise_3.py
@@ -1,5 +1,7 @@
 import string
 
+from functools import reduce
+
 alphabet = string.ascii_lowercase + " "
 letters = dict(enumerate(alphabet))
 
@@ -7,12 +9,14 @@ message = "hi my name is caesar"
 
 
 def caesar(message, encryption_key):
-    encoded_message = ""
     encoding = {char: (value + encryption_key) % 27 for (value, char) in letters.items()}
-    for letter in message:
-        encoded_message += letters[encoding[letter]]
-    return encoded_message
-
+    return reduce(
+        lambda x, y: x + y,
+        map(
+            lambda char: letters[encoding[char]],
+            message
+        )
+    )
 
 encoded_message = caesar(message, 3)
 
