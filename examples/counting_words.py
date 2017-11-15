b07d0dd89ce785210a565b6b3edfda1d9312d00b
ahasani <albin.hasani@ikubinfo.al>
2017-11-15 01:12:10 +0100

    counting words

---
+++ examples/counting_words.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 00:59:21 2017
+
+@author: ahasani
+"""
+from collections import Counter
+
+text = "dummy text for counting 'Dummy' words. dummy code - dummy text."
+
+
+def count_words(text):
+    """
+    Count the number of times each word occurs in text
+    """
+    text = text.lower();
+    skips = [".", ",", ";", ":", "'", '"']
+    for ch in skips:
+        text = text.replace(ch, "")
+
+    word_counts = Counter(text.split(" "))
+    return word_counts
+
+
+print(count_words(text))
