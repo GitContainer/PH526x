b6dbdd852b6b7c0c61aa8c337fce4e278c5e55e3
ahasani <albin.hasani@ikubinfo.al>
2017-11-19 01:54:17 +0100

    add correlation and visualize

--- week4/classifying_whiskies/inspecting_data.py
+++ week4/classifying_whiskies/inspecting_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 whisky = pd.read_csv("whiskies.txt")
 whisky["Region"] = pd.read_csv("regions.txt")
@@ -12,3 +13,11 @@ print(whisky.columns)
 flavors = whisky.iloc[:, 2:14]
 
 print(flavors.columns)
+
+corr_flavors = pd.DataFrame.corr(flavors)
+print(corr_flavors)
+
+plt.figure(figsize=(10, 10))
+plt.pcolor(corr_flavors)
+plt.colorbar()
+plt.savefig("correlation_flavors.pdf")
