c9abae7a0afcf94abd026e6b2f652a2ed5a05469
ahasani <albin.hasani@ikubinfo.al>
2017-11-19 01:47:34 +0100

    inspecting data

--- week4/classifying_whiskies/inspecting_data.py
+++ week4/classifying_whiskies/inspecting_data.py
@@ -5,3 +5,10 @@ whisky = pd.read_csv("whiskies.txt")
 whisky["Region"] = pd.read_csv("regions.txt")
 
 print(whisky.head())
+print(whisky.iloc[0:10])
+print(whisky.iloc[5:10, 0:5])
+print(whisky.columns)
+
+flavors = whisky.iloc[:, 2:14]
+
+print(flavors.columns)
